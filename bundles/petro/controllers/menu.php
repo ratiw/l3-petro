d02828c632f61a27e327e0a60a3b21228b394563
Rati Wannapanop <rati.wannapanop@gmail.com>
2013-06-28 12:04:41 +0700

    add default actions to menu controllers

--- bundles/petro/controllers/menu.php
+++ bundles/petro/controllers/menu.php
@@ -8,7 +8,7 @@ class Petro_Menu_Controller extends Petro_App_Controller
 
 	// public $form_columns = array();
 
-	public $grid_columns = array('seq', 'group', 'name', 'title', 'title_en', 'link', 'has_sub', 'parent', 'level', 'active');
+	public $grid_columns = array('seq', 'group', 'name', 'title', 'title_en', 'link', 'has_sub', 'parent', 'level', 'active', '_actions_');
 
 
 	protected function setup_index(&$grid)
